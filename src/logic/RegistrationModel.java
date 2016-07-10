8a6da7362190392dc02a918886754e6fc7b0f1b5
TkachenkoMax <manutd20x@gmail.com>
2016-07-11 01:54:24 +0300

    First commit from teamlead: RegistrationModel class

--- src/logic/RegistrationModel.java
+++ src/logic/RegistrationModel.java
@@ -4,10 +4,55 @@ public class RegistrationModel {
 
     private String nick;
     private String password;
+    private String country;
+    private String city;
+    private int age;
+    private String name;
+    private String surname;
 
     public RegistrationModel(String nick, String password) {
         this.nick = nick;
         this.password = password;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = Integer.parseInt(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
     }
 
     public String getNick() {
