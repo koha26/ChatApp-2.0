4e1b90087cc3580fc0720a7da7b887f4496c2916
TkachenkoMax <manutd20x@gmail.com>
2016-07-17 23:53:12 +0300

    1 шрифт

--- src/gui/Fonts.java
+++ src/gui/Fonts.java
@@ -3,6 +3,6 @@ package gui;
 import java.awt.*;
 
 public class Fonts {
-
     public static final Font typingFont = new Font("Century Gothic", Font.PLAIN, 16);
+    public static final Font nickFont = new Font("Berlin Sans FB Demi", Font.PLAIN, 32);
 }
